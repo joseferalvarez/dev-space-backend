6167fad2da21a7596ae99e5e147a26e05e88947f
Jose Fernando Álvarez Romero <joseferalvarezromero@gmail.com>
2024-08-29 22:55:11 +0200

    database connection created

--- database.py
+++ database.py
@@ -1,1 +1,15 @@
 import mariadb
+
+def connect(config):
+  try:
+    connection = mariadb.connect(
+      user = config["user"],
+      password = config["password"],
+      database = config["database"],
+      host = config["host"],
+      port = int(config["port"])
+    )
+  except mariadb.Error as e:
+    print(e)
+
+  return connection
--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI;
+from database import connect
+import yaml
 
 app = FastAPI();
+
+with open("dev.config.yml", "r") as file:
+  config = yaml.safe_load(file)
+
+connection = connect(config["database"])
+cursor = connection.cursor()
+
+cursor.execute("INSERT INTO languages (language_name, slug, iso) VALUES ('Spanish', 'spanish', 'es');")
+cursor.execute("SELECT * FROM languages")
+connection.commit()
+connection.close()
+
+for language in cursor:
+  print(language)
 
 @app.get("/")
 def get_test():
